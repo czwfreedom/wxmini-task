21fef82533d28a0e4f3daff909a2fe3a21d26f4d
castlechen <chenzhiwu@laiqutech.com>
2026-08-11 22:53:46 +0800

    1、调整输入框高度。

--- miniprogram/core/context.ts
+++ miniprogram/core/context.ts
@@ -22,6 +22,7 @@ export namespace Context {
   }
 
   export function isNamed(): boolean {
+    // if (1) return false; // DEBUGGER;
     const user = getUser();
     return !!user?.name && user.name !== '微信用户';
   }
