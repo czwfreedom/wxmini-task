921aeca33f0744b3d8ec3b6505af48dacfde1d58
castlechen <chenzhiwu@laiqutech.com>
2026-08-14 16:28:29 +0800

    1、加配置。

--- miniprogram/server/config.ts
+++ miniprogram/server/config.ts
@@ -9,6 +9,7 @@ import { Routine } from './routine';
 export namespace Config {
   export interface Info extends Entity.Info {
     type: number;
+    detail?: string;
     used?: number;
     proirity?: number;
     ref?: string;
@@ -21,12 +22,45 @@ export namespace Config {
     RoutineTemplate = 1, // 日程模板
   }
 
+  export interface Template extends Info {
+    items: Partial<Routine.Info>[];
+  }
+
+  //   export const sTemplates: Template[] = [
+  //     {
+  //       id: '',
+  //       name: '学霸日常三件套',
+  //       detail: '每天必做的基础学习，不多不少刚刚好',
+  //       items: [
+  //         { category: Routine.Category.Reading, duration: 1800000 },
+  //         { category: Routine.Category.Homework, duration: 2700000 },
+  //         { category: Routine.Category.Handwriting, duration: 1800000 },
+  //       ],
+  //     },
+  //     {
+  //       id: '',
+  //       name: '小小探索家',
+  //       detail: '练体魄、问问题、看世界',
+  //       items: [
+  //         { category: Routine.Category.Reading, duration: 2700000 },
+  //         { category: Routine.Category.QA, duration: 900000 },
+  //         { category: Routine.Category.Exercise, duration: 1800000 },
+  //       ],
+  //     },
+  //   ] as any;
+
   export interface ListRequest extends Info {
     brief: boolean;
   }
 
-  export function parseTemplate(data: Info[]): Partial<Routine.Info[]> {
-    return data.map((o) => Utils.parseJson(o.value, true));
+  export function parseTemplate(data: Info[]): Partial<Template[]> {
+    const result: Template[] = [];
+    for (const item of data) {
+      const v = item as Template;
+      v.items = Utils.parseJson(item.value) || [];
+      if (v?.items?.length) result.push(v);
+    }
+    return result;
   }
 
   export async function list(data: Partial<ListRequest>): Promise<number | Info[]> {
